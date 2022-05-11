dd768515ce2613672dfb6b076522ecd7661c2be0
x-yl <kylepereira@mail.com>
2022-05-11 12:46:16 +0100

    New stuff!
    It's cool, hope u like guys

--- index.js
+++ index.js
@@ -53,25 +53,3 @@ particlesJS("particles-js", {
     },
     retina_detect: true
 });
-
-let d = new Date('2022-06-23T00:12:00') - new Date();
-const spans = Array.from(document.querySelectorAll('span')).reverse();
-const s = [1000,60,60,24];
-
-const vset = (e,t,c) => {
-	const m = c ? t % c : t;
-	e.setAttribute('b', m < 10 ? '0' + m : m);
-	e.classList.remove('ping');
-	setTimeout(() => e.classList.add('ping'), 10);
-	return m;
-};
-const calc = (t,i=0,b=0) => {
-	if (!s[i]) return;
-	t = opti(t,s[i]);
-	if (vset(spans[i],t,s[i+1])==s[i+1]-1 || b) calc(t,i+1,b);
-}
-
-const count = (b=0) => (d -= 1000) && calc(d,0,b);
-const opti = (v,n) => (v - (v % n)) / n;
-
-setTimeout(() => !count(1) && setInterval(count, 1000), d % 1000);
