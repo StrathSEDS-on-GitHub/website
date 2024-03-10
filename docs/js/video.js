c44c8d2eb7a902b8b70858bc1a90c6aae6de302d
Calum <57592605+calum-s@users.noreply.github.com>
2024-03-10 00:54:52 +0000

    Blog and general formatting fixes (#5)
    * Blog and general formatting fixes
    
    ---------
    
    Co-authored-by: Kyle Pereira <kyle@kylepereira.me>

--- docs/js/video.js
+++ docs/js/video.js
@@ -1,17 +1,41 @@
-const main = () => {
+const main = async () => {
     let images = [];
 
-    fetch("assets/launch2.tar").then(response => {
-        return response.arrayBuffer();
-    })
-        .then(buffer => untar(buffer))
-        .then(files => {
-            files.forEach((file) => {
-                let img = new Image();
-                img.src = file.getBlobUrl();
-                images.push(img);
-            });
-        });
+    // Check if browser is mobile or desktop
+    let file = "launch-horz.tar"
+    if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
+        console.log("Using mobile launch images")
+        file = "launch-vert.tar"
+    } else {
+        console.log("Using desktop launch images")
+    }
+
+    let resp = await fetch("assets/" + file);
+    let length = parseInt(resp.headers.get("Content-Length"));
+
+    let loaded = 0
+
+    resp = new Response(new ReadableStream({
+        async start(controller) {
+            const reader = resp.body.getReader();
+
+            while (true) {
+                const { done, value } = await reader.read();
+                if (done) break;
+                loaded += value.byteLength;
+                document.getElementById("loading-bar").value = loaded / length * 100;
+                controller.enqueue(value);
+            }
+            controller.close();
+        }
+    }));
+
+    let files = await untar(await resp.arrayBuffer());
+    files.forEach((file) => {
+        let img = new Image();
+        img.src = file.getBlobUrl();
+        images.push(img);
+    });
 
     let jumbotronFront = document.getElementById("jumbotron-content-back");
     let jumbotronBack = document.getElementById("jumbotron-content");
@@ -38,8 +62,8 @@ const main = () => {
             next.style.zIndex = 1000;
         });
         window.requestAnimationFrame(frame);
-        next = next == jumbotronFront ? jumbotronBack : jumbotronFront; 
-        other = other == jumbotronFront ? jumbotronBack : jumbotronFront; 
+        next = next == jumbotronFront ? jumbotronBack : jumbotronFront;
+        other = other == jumbotronFront ? jumbotronBack : jumbotronFront;
     }
     window.requestAnimationFrame(frame);
 }
